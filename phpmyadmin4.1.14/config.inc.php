91b542d7b1db56698f9432dfd525025b7bdd815a
prince bansal <eklavyabhar.2009@gmail.com>
2015-11-04 09:31:26 +0530

    config.php

--- phpmyadmin4.1.14/config.inc.php
+++ phpmyadmin4.1.14/config.inc.php
@@ -1,43 +1,74 @@
 <?php
+
 /* vim: set expandtab sw=4 ts=4 sts=4: */
+
 /**
+
  * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
+
+ * manual configuration. For easier setup you can use setup
+/
  *
  * All directives are explained in documentation in the doc/ folder
- * or at <http://docs.phpmyadmin.net/>.
- *
- * @package PhpMyAdmin
- */
+ 
+* or at <http://docs.phpmyadmin.net/>.
+ 
+*
+ 
+* @package PhpMyAdmin
+ 
+*/
+
 
 /*
  * This is needed for cookie based authentication to encrypt password in
- * cookie
- */
-$cfg['blowfish_secret'] = 'a8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+ 
+* cookie
+ 
+*/
+$cfg['blowfish_secret'] = 'a8b7c6d'; 
+/* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
 
 /*
  * Servers configuration
  */
+
 $i = 0;
+
 
 /*
  * First server
  */
+
 $i++;
+
 /* Authentication type */
-$cfg['Servers'][$i]['verbose'] = 'mysql wampserver';
-//$cfg['Servers'][$i]['auth_type'] = 'cookie';
+
+//$cfg['Servers'][$i]['verbose'] = 'mysql wampserver';
+
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+//
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = '';
+
+//$cfg['Servers'][$i]['user'] = 'root';
+
+//$cfg['Servers'][$i]['password'] = '';
+
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '127.0.0.1';
+
+$cfg['Servers'][$i]['host'] = 'ap-cdbr-azure-southeast-a.cloudapp.net';
+
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
+
 $cfg['Servers'][$i]['compress'] = false;
+
 /* Select mysql if your server does not have mysqli */
+
 $cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+
 
 
 
@@ -47,6 +78,7 @@ $cfg['Servers'][$i]['AllowNoPassword'] = true;
  
 // No warning on  pmadb tables
 $cfg['PmaNoRelation_DisableWarning'] = true;
+
 
 
 ?>
