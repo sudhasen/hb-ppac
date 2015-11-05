16d0c5953c4afbdf9070fd68784272c2c03db203
prince bansal <eklavyabhar.2009@gmail.com>
2015-11-06 02:59:35 +0530

    changed include in uploads.php

--- upload.php
+++ upload.php
@@ -15,16 +15,16 @@ if(isset($_POST["submit"])) {
     
 }
 if(isset($_POST["level"])) {
-    
+        
         $level=$_POST["level"];
         $uploadOk = 1;
     
 }
 // Check if file already exists
-if (file_exists($target_file)) {
+/*if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
-}
+}*/
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
     echo "Sorry, your file is too large.";
